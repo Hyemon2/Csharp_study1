2c549d4e327bec08dc53dd0848130b91d98fa694
Hyewon Seo <Hyewooon826@gmail.com>
2025-03-21 01:40:27 +0900

    Add files via upload

---
+++ ex3-1/Program.cs
@@ -0,0 +1,48 @@
+﻿class ex32
+{
+    static void Main()
+    {
+        int num = 0;
+        Console.WriteLine("소수 판별 프로그램 \n소수가 입력 될떄까지 계속 반복합니다.");
+        while (true)
+        {
+            Console.Write("숫자를 입력하세요 : ");
+            num = int.Parse(Console.ReadLine());
+            bool sosu = true;
+            if (num == 2)
+            {
+                sosu = true;
+            }
+            else if (num % 2 == 0)
+            {
+                sosu = false;
+            }
+            else
+            {
+                for (int i = 3; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        sosu = false;
+                        break;
+                    }
+                    else
+                    {
+                        sosu = true;
+                    }
+                }
+            }
+            if (sosu)
+            {
+                Console.WriteLine($"{num}은 소수입니다.");
+                Console.WriteLine("프로그램을 종료합니다.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"{num}은 소수가 아닙니다.");
+            }
+        }
+        Console.ReadLine();
+    }
+}
---
+++ ex3/Program.cs
@@ -0,0 +1,47 @@
+﻿class ex3
+{
+    static void Main()
+    {
+        int num = 0;
+        bool sosu = true;
+        Console.Write("양의 정수를 입력하세요");
+        num = int.Parse(Console.ReadLine());
+        if (num <= 2)
+        {
+            Console.WriteLine($"{num}은 소수가 아닙니다. ");
+            return;
+        }
+        else
+        {
+            if (num % 2 == 0)
+            {
+                sosu = false;
+                return;
+            }
+            else
+            {
+                for (int i = 3; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        sosu = false;
+                        break;
+                    }
+                    else
+                    {
+                        sosu = true;
+                    }
+                }
+            }
+        }
+        if (sosu)
+        {
+            Console.WriteLine($"{num}은 소수입니다.");
+        }
+        else
+        {
+            Console.WriteLine($"{num}은 소수가 아닙니다.");
+        }
+        Console.ReadLine();
+    }
+}
